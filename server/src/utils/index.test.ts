ff0347c38a173e404a782bd34db4e77ed975a162
Yi Lu <luyi985@gmail.com>
2019-12-03 07:25:00 +1100

    test: some more tests

--- server/src/utils/index.test.ts
+++ server/src/utils/index.test.ts
@@ -16,4 +16,43 @@ describe('deep', () => {
 		const t = ['abc', 123, null];
 		expect(deep(t, vf, kf)).toEqual(['abc', 123, null]);
 	});
+
+	it('with nested array', () => {
+		const t = [
+			'abc',
+			123,
+			null,
+			{
+				ABC: 'abc',
+				EoF: {
+					Inner: 'inner',
+				},
+				Comments: [],
+				Fields: [
+					{
+						Name: 'name1',
+					},
+					{ Name: 'name2' },
+				],
+			},
+		];
+		expect(deep(t, vf, kf)).toEqual([
+			'abc',
+			123,
+			null,
+			{
+				abc: 'abc',
+				eof: {
+					inner: 'inner',
+				},
+				comments: [],
+				fields: [
+					{
+						name: 'name1',
+					},
+					{ name: 'name2' },
+				],
+			},
+		]);
+	});
 });
